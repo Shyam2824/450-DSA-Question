0fe2dfb61490b1db8bdfc3952cac16f7e8100bb2
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-02-13 14:49:59 +0530

    find max and min in class

--- a_02max_minArray.java
+++ a_02max_minArray.java
@@ -1,37 +1,12 @@
 import java.util.Scanner;
-// class max_min{
+class max_min{
 
-//     public static void  getMinMax(long arr[], long n){
-//         // Long max= Long.MIN_VALUE;
-//         // Long min= Long.MAX_VALUE;
-
-//         // for(int i=0;i<n;i++){
-//         //     if(arr[i]>max){
-//         //         max=arr[i];
-//         //     }
-//         //     if(arr[i]<min){
-//         //         min=arr[i];
-//         //     }
-//         // }
-//     }
-// }
-public class a_02max_minArray{
-    
-
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the Element");
-        int k= sc.nextInt();
-        int arr[]= new int[k];
-
-        System.out.println("Enter value");
-        for(int i=0;i<k;i++){
-            arr[i]= sc.nextInt();
-        }
-        int max= arr[0];
-        int min= arr[0];
-
-        for(int i=0;i<arr.length;i++){
+    public void  getMinMax( long arr[],long n){
+        Long max= Long.MIN_VALUE;
+        Long min= Long.MAX_VALUE;
+        
+        
+        for(int i=0;i<n;i++){
             if(arr[i]>max){
                 max=arr[i];
             }
@@ -39,8 +14,40 @@ public class a_02max_minArray{
                 min=arr[i];
             }
         }
-        System.out.println("Maximum element: " + max);
-        System.out.println("Minimum element: " + min);
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    
+    }
+}
+public class a_02max_minArray{
+    
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Element");
+        int k= sc.nextInt();
+        long[] arr= new long[k];
+
+        System.out.println("Enter value");
+        for(int i=0;i<k;i++){
+            arr[i]= sc.nextInt();
+        }
+
+        max_min m= new max_min();
+        m.getMinMax(arr, k);
+        // int max= arr[0];
+        // int min= arr[0];
+
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>max){
+        //         max=arr[i];
+        //     }
+        //     if(arr[i]<min){
+        //         min=arr[i];
+        //     }
+        // // }
+        // System.out.println("Maximum element: " + max);
+        // System.out.println("Minimum element: " + min);
        
     //    for(int i=0;i<k;i++){
     //    System.out.println(arr[k]);
