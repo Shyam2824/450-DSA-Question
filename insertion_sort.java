4ca3c3faf571bac165676441cb5aa36835fe7300
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-04-15 16:45:58 +0530

    insertion sort

--- insertion_sort.java
+++ insertion_sort.java
@@ -2,32 +2,35 @@ import java.util.Scanner;
 
 public class insertion_sort {
     public static void main(String[] args) {
-        int n,i,j,temp =0;
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter no. of items");
+        int n, i, j, temp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of items:");
         n = sc.nextInt();
-        int a[]= new int[n];
+        int a[] = new int[n];
 
-        System.out.println("Enter Array");
-        for(i=0;i<n;i++){
-            a[i]= sc.nextInt();
+        System.out.println("Enter Array:");
+        for (i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
 
-        for(i=1;i<=n;i++)
-        {
-            temp=a[i];
-            j=i-1;
-            while (temp<a[j] && j>-1) 
-            {
-                a[j+1] = a[j];
-                j= j-1;
+        // Insertion Sort
+        for (i = 1; i < n; i++) {
+            temp = a[i];
+            j = i - 1;
+
+            while (j >= 0 && a[j] > temp) {
+                a[j + 1] = a[j];
+                j = j - 1;
             }
-            a[j+1] = temp;
+            a[j + 1] = temp;
         }
-        for(i=0;i<n; i++){
-            System.out.println(a[i]);
+
+        System.out.println("Sorted Array:");
+        for (i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 }
+
 
 
